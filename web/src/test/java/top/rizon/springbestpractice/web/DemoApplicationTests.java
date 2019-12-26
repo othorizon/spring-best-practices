3a8d6a9fc09f843f5c5b9f8420bb71a88fd214d2
othorizon <othorizon@users.noreply.github.com>
2019-12-26 16:36:00 +0800

    fix junit dependency

--- web/src/test/java/top/rizon/springbestpractice/web/DemoApplicationTests.java
+++ web/src/test/java/top/rizon/springbestpractice/web/DemoApplicationTests.java
@@ -1,13 +1,16 @@
 package top.rizon.springbestpractice.web;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
